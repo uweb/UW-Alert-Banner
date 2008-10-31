5dd1911026d28237f5010db330541c30f8be5b0f
topherh <cheiland@uw.edu>
2008-10-31 20:32:05 +0000

    Separated out emergency functions to emergency.js, putting variables in the alert.js and includes to load first.  Currently work, although the hide function takes two runs before it works.

--- alert.js
+++ alert.js
@@ -22,89 +22,12 @@
  * Include our javascript object script, the wonderful Prototype... and friends
  *---------------------------*/
 
-var alert = '1';
-var strStyle = alert ? 'uwalert_red.css' : 'uwalert_orange.css';
- 
-document.write('<scr' + 'ipt type="text\/javascript" src="prototype.js"><\/script>' +
-'<link href="'+ strStyle +'" rel="stylesheet" type="text\/css" \/>' +
+// Might have to do some additional work here
+document.write('<scr' + 'ipt type="text\/javascript" src="prototype.js"><\/script>');
+document.write('<scr' + 'ipt type="text\/javascript" src="emergency.js"><\/script>');
+
+// Dynamically set the next two
+var alert = 'red';
+var strStyle = alert == 'red' ? 'uwalert_red.css' : 'uwalert_orange.css';
+document.write('<link href="'+ strStyle +'" rel="stylesheet" type="text\/css" \/>' +
 '<sty' + 'le type="text\/css"><!-- body { margin: 0px; } --><\/style>');
-
-function addElement() {
-  var body = document.getElementsByTagName('body')[0];
-  
-  var newdiv = document.createElement('div');
-  var divIdName = 'alertMessage';
-  
-  newdiv.setAttribute('id',divIdName);
-  //newdiv.innerHTML = 'Element Number '+num+' has been added! <a href=\'#\' onclick=\'removeElement('+divIdName+')\'>Remove the div "'+divIdName+'"</a>';
-  body.appendChild(newdiv);
-}
-
-function get_cookie(cookie_name)
-{
-    var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
-
-    if ( results )
-        return ( unescape ( results[2] ) );
-    else
-        return null;
-}
-
-function hideit(id)
-{
-    $('alertBox').hide();
-    
-    var cookie_date = new Date();
-    var expdate = cookie_date.getTime();
-    expdate += 3600*1000; //expires in 1 hour(milliseconds) 
-    cookie_date.setTime(expdate);
-    
-    document.cookie = 'hide=yes;expires=' +
-        cookie_date.toGMTString();
-
-    // Dirty little Hack - need to find better solution before launch
-    location.reload(true);
-}
-
-function getMessage(status)
-{
-    //var sGetMsgUrl = hide ? 'get_message.php?hide=1' : 'get_message.php';
-    var sGetMsgUrl = 'get_message.php';
-    
-    var message = new Ajax.PeriodicalUpdater('alertMessage', sGetMsgUrl, {
-	    method: 'post', // using POST to combat IE caching,
-	    frequency: 10
-	});
-    
-    if (status == 'stop')
-    {
-        message.stop();
-    }
-    else
-    {
-        message.start();
-    }
-    document.write(status);
-    
-    //return message;
-}
-
-function displayAlert()
-{
-    var hide = get_cookie("hide");
-    if (hide)
-    {
-        addElement();
-        getMessage('stop');
-    }
-    else
-    {
-        addElement();
-        getMessage('start');
-    }
-}
-// output the HTML block we receive from the php script
-// if (!hide)
-// {
-    // document.write('<div id="alertMessage"><\/div>');
-// }
--- emergency.js
+++ emergency.js
@@ -1,22 +1,71 @@
-        function hideit(id)
-        {
-        	//el = document.getElementById(id);
-        	//el.style.display = 'none';
-            
-            $('alertBox').hide();
+function getMessage()
+{
+    //var sGetMsgUrl = hide ? 'get_message.php?hide=1' : 'get_message.php';
+    var sGetMsgUrl = 'get_message.php';
+    
+    var message = new Ajax.PeriodicalUpdater('alertMessage', sGetMsgUrl, {
+	    method: 'post', // using POST to combat IE caching,
+	    frequency: 3
+	});
 
-            var sGetMsgUrl = 'get_message.php?hide=1';
-        }
-		
-        function getMessage()
-        {
-			var sGetMsgUrl = 'get_message.php';
-			new Ajax.PeriodicalUpdater('MessageContainer', sGetMsgUrl, {
-			    method: 'post', // using POST to combat IE caching,
-                //insertion: Insertion.Top,
-			    frequency: 10,
-			});
-            
-			document.write('<div id="alertBox"><div id="alertBoxText"><h1>Campus Alert:</h1><p id="MessageContainer"></p></div><a href="#" onclick="javascript:hideit(\'alertBox\')"><img src="close.gif" name="xmark" width="10" height="10" id="xmark" /></a><div id="clearer"></div></div>');
-		
-		}
+    var hide = get_cookie("hide");
+    if (hide)
+        message.stop();
+    return message;
+}
+
+var oMessage = getMessage();
+
+function addElement() 
+{
+  var body = document.getElementsByTagName('body')[0];
+  
+  var newdiv = document.createElement('div');
+  var divIdName = 'alertMessage';
+  
+  newdiv.setAttribute('id',divIdName);
+  
+  body.insertBefore(newdiv, body.firstChild);
+  //body.appendChild(newdiv);
+}
+
+function get_cookie(cookie_name)
+{
+    var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
+
+    if ( results )
+        return ( unescape ( results[2] ) );
+    else
+        return null;
+}
+
+function hideit(id)
+{
+    $('alertBox').hide();
+    
+    var cookie_date = new Date();
+    var expdate = cookie_date.getTime();
+    expdate += 3600*1000; //expires in 1 hour(milliseconds) 
+    cookie_date.setTime(expdate);
+    
+    document.cookie = 'hide=yes;expires=' +
+        cookie_date.toGMTString();
+
+    oMessage.stop();
+}
+
+
+function displayAlert()
+{
+    var hide = get_cookie("hide");
+    
+    if (hide)
+    {
+        oMessage.stop();
+    }
+    else
+    {
+        addElement();
+        oMessage.start();
+    }
+}
