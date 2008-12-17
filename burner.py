0c5a8bb06c277f8d76ccf9b70d58a557671b74a6
topherh <cheiland@uw.edu>
2008-12-17 18:31:08 +0000

    Added clear debug section, cleared up logic and looping in wonky section.

--- burner.py
+++ burner.py
@@ -8,8 +8,12 @@ needed.  If no alert, then just output skeleton file.
 
 import os, re
 import uwlibweb, feedparser
-#import pprint
 from datetime import timedelta, datetime
+
+################ DEBUG ####################
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+################ DEBUG ####################
 
 strHeader = """
 /*  University of Washington - Alert 1.0 Beta
@@ -20,7 +24,7 @@ strHeader = """
  *  <html>
  *  <head>
  *  <title>Page Title</title>
- *  <script type="text/javascript" src="http://depts.washington.edu/uweb/scripts/alert.js"></script>
+ *  <script type="text/javascript" src="http://depts.washington.edu/uweb/emergency/alert.js"></script>
  *  </head>
  *  <body>
  *  
@@ -48,37 +52,18 @@ ORANGE = 9
 BLUE = 10
 STEEL = 12
 
-strTestHTMLContent = """
-<div id="alertBox">
-<div id="alertBoxText">
-        <h1>Campus Alert:</h1>
-        <p>There is no emergency.  For additional information, 
-        please visit <a href="http://emergency.washington.edu">emergency.washington.edu</a></p>
-    </div>
-    <div id="clearer"></div>
-</div> """
-
-strAddElement = """
-// addElement - display HTML on page right below the body page
-// don't want the alert to show up anywhere
-function addElement(strAlertMessageHTML)
-{
-  var bodyTag = document.getElementsByTagName('body')[0];
-
-  var newDiv = document.createElement('div');
-  var divIdName = 'alertMessage';
-  
-  newDiv.setAttribute('id',divIdName);
-  newDiv.innerHTML = strAlertMessageHTML;
-
-  bodyTag.insertBefore(newDiv, bodyTag.firstChild);
-} """
-
-###!!DEBUG!!###
-####pp = pprint.PrettyPrinter(indent=4)####
+# strTestHTMLContent = """
+# <div id="alertBox">
+# <div id="alertBoxText">
+        # <h1>Campus Alert:</h1>
+        # <p>There is no emergency.  For additional information, 
+        # please visit <a href="http://emergency.washington.edu">emergency.washington.edu</a></p>
+    # </div>
+    # <div id="clearer"></div>
+# </div> """
 
 # We want to make sure there are no problems with a lock of a post
-# If there are any problems, just set the date to zero
+# If there are any problems, just set the entry to zero
 hashAlert = dict() # {'red': '', 'orange': '', 'blue': '', 'steel': ''}
 try:
     strFeed = feedparser.parse(uwlibweb.getFeedData(RED))
@@ -107,46 +92,50 @@ try:
     hashAlert['steel'] = {'feed': strFeed, 'date': strDate}
 except:
     hashAlert['steel'] = ''
-    
-#pp.pprint(hashAlert)    
-    
-# No alert is a good Alert
-strAlertStatus = 0
-################### StartWeird
 
+# No alert is a good Alert
+strAlert = ''
+strStyle = ''
+################### Start Less Weird Than Yesterday
+
+#Example Data
+# # # # hashAlert{
+    # # # # 'color': {'feed': '', 'date', ''},
+    # # # # 'color': {'feed': '', 'date', ''},
+    # # # # 'color': {'feed': '', 'date', ''},
+    # # # # 'color': {'feed': '', 'date', ''},
+    # # # # }
+    
 hashDates = {}
 
 for strKey,strValue in hashAlert.items():
     if strValue != '':
-        for strKey2,strValue2 in hashAlert[strKey].items():
-            hashDates[strKey] = hashAlert[strKey]['date']
-        
+        hashDates[strKey] = hashAlert[strKey]['date']
+
+#pp.pprint(hashDates)
+#pp.pprint(hashAlert)
+
 # hashDates will only contain the colors with dates        
 if hashDates:
     strHighDate = uwlibweb.getHighest(hashDates)
-    for strKey,strValue in hashAlert.items():
+    for strAlertColor,strValue in hashAlert.items():
         # Avoid posting a blank alert
-        if hashAlert[strKey] != '':
-            for strKey2,strValue2 in hashAlert[strKey].items():
-                if hashAlert[strKey]['date'] == strHighDate:
-                    strAlertColor = strKey
-                    strAlertStatus = 1
+        if strValue != '':
+            if hashAlert[strAlertColor]['date'] == strHighDate:
+                strAlert = hashAlert[strAlertColor]['feed']
+                strStyle = 'uwalert_' + strAlertColor + '.css'
 
-strAlert = 0
-
-if strAlertStatus:
-    strAlert = hashAlert[strAlertColor]['feed']
-################### End Weird
+################### Start Less Weird Than Yesterday
 
 if strAlert:
     # A lack of color should fail before we get here
     # We are setting this value manually - we can trust it
-    strStyle = 'uwalert_' + strAlertColor + '.css'
+    # strStyle = 'uwalert_' + strAlertColor + '.css'
 
     # Take newest item and display
-    strTitle = strAlert.entries[0].title.decode('iso-8859-1') # don't trust encoding
+    strTitle = strAlert.entries[0].title.encode("utf-8") # don't trust encoding
     strLink = 'http://emergency.washington.edu/'
-    strDesc = strAlert.entries[0].description.decode('iso-8859-1') # don't trust encoding
+    strDesc = strAlert.entries[0].description.encode("utf-8") # don't trust encoding
     # Encode vs. Decode article
     #http://mail.python.org/pipermail/python-list/2004-August/275972.html
     strDate = strAlert.entries[0].date
@@ -154,10 +143,11 @@ if strAlert:
 
     strFormatDate = datetime(strParsedDate.tm_year, strParsedDate.tm_mon, strParsedDate.tm_mday, strParsedDate.tm_hour, strParsedDate.tm_min, strParsedDate.tm_sec)
 
-    strContent = strDesc[:180] + '... '
-
     if strDesc and strTitle:
+        strContent = strDesc[:180] + '... '
         strContent += '<a href="' + strLink + '" title ="' + strTitle + '">More Info</a> &gt;&gt;'
+    else:
+        strContent = 'Whoops'
 
     strHTMLContent = """
 <div id="alertBox">
@@ -168,6 +158,22 @@ if strAlert:
     <div id="clearer"></div>
 </div> """ % (strContent)
 #strFormatDate.strftime("%A, %B %d"))        # Enable once server time is fixed on spokane
+
+    strAddElement = """
+// addElement - display HTML on page right below the body page
+// don't want the alert to show up anywhere
+function addElement(strAlertMessageHTML)
+{
+  var bodyTag = document.getElementsByTagName('body')[0];
+
+  var newDiv = document.createElement('div');
+  var divIdName = 'alertMessage';
+  
+  newDiv.setAttribute('id',divIdName);
+  newDiv.innerHTML = strAlertMessageHTML;
+
+  bodyTag.insertBefore(newDiv, bodyTag.firstChild);
+} """
 
     strContent = """
 document.write('<scr' + 'ipt type="text\/javascript" src="http://depts.washington.edu/uweb/emergency/prototype.js"><\/script>' +
@@ -194,22 +200,22 @@ function displayAlert(strMode)
     strOutput = strHeader + strContent + "\n";
 
 # This may only be used for me - still good to have
-strTestStyle = 'uwalert_steel.css'
-strTestContent = """
-document.write('<scr' + 'ipt type="text\/javascript" src="http://depts.washington.edu/uweb/emergency/prototype.js"><\/script>' +
-'<scr' + 'ipt type="text\/javascript" src="http://depts.washington.edu/uweb/emergency/scriptaculous.js?load=effects"><\/script>');
+# strTestStyle = 'uwalert_steel.css'
+# strTestContent = """
+# document.write('<scr' + 'ipt type="text\/javascript" src="http://depts.washington.edu/uweb/emergency/prototype.js"><\/script>' +
+# '<scr' + 'ipt type="text\/javascript" src="http://depts.washington.edu/uweb/emergency/scriptaculous.js?load=effects"><\/script>');
 
-document.write('<link href="http://depts.washington.edu/uweb/emergency/%s" rel="stylesheet" type="text\/css" \/>' +
-    '<sty' + 'le type="text\/css"><!-- body { margin: 0; padding: 0; } --><\/style>');
+# document.write('<link href="http://depts.washington.edu/uweb/emergency/%s" rel="stylesheet" type="text\/css" \/>' +
+    # '<sty' + 'le type="text\/css"><!-- body { margin: 0; padding: 0; } --><\/style>');
 
-// displayAlert - grab HTML to display message 
-function displayAlert()
-{
-    var strAlertMessageHTML = '%s';
-    // Can we pass to the following function?
-    addElement(strAlertMessageHTML);
-}
-""" % (strTestStyle, re.escape(strTestHTMLContent))
+# // displayAlert - grab HTML to display message 
+# function displayAlert()
+# {
+    # var strAlertMessageHTML = '%s';
+    # // Can we pass to the following function?
+    # addElement(strAlertMessageHTML);
+# }
+# """ % (strTestStyle, re.escape(strTestHTMLContent))
 
 #uwlibweb.saveAlert('alert.js', strContent)
 # Pushing out real alert
@@ -221,13 +227,13 @@ try:
 except Exception, strError:
     print "Error Writing to File %s because %s" % (strFileout, strError)
 
-# Alert for my test purposes 
-strOutput = strHeader + strTestContent + strAddElement + "\n";
-#uwlibweb.saveAlert('alert-test.js', strContent)
-try:
-    strFileout = '/rc22/d10/uweb/public_html/emergency/alert-test.js'
-    objFile = open(strFileout, "w")
-    objFile.write(strOutput)
-    objFile.close()
-except Exception, strError:
-    print "Error Writing to File %s because %s" % (strFileout, strError)
+# # Alert for my test purposes 
+# strOutput = strHeader + strTestContent + strAddElement + "\n";
+# #uwlibweb.saveAlert('alert-test.js', strContent)
+# try:
+    # strFileout = '/rc22/d10/uweb/public_html/emergency/alert-test.js'
+    # objFile = open(strFileout, "w")
+    # objFile.write(strOutput)
+    # objFile.close()
+# except Exception, strError:
+    # print "Error Writing to File %s because %s" % (strFileout, strError)
