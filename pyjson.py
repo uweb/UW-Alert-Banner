fa72cb4cb35542b2151d27662f9f526a72dce817
Chris Heiland <cheiland@uw.edu>
2011-08-16 10:22:44 -0700

    Pretty print is finally working

--- pyjson.py
+++ pyjson.py
@@ -2,7 +2,7 @@
 
 import urllib2, json
 
-#strURL = 'http://emergency.washington.edu/?json=1'
+strURL = 'http://emergency.washington.edu/?json=1'
 #oFile = urllib2.urlopen(strURL)
 #oJ = json.loads( oFile.read() )
 
@@ -10,11 +10,10 @@ import urllib2, json
 #    for category in post['categories']:
 #        print category['id']
 
-strURL = 'http://emergency.washington.edu/category/emergency-links/?json=1'
+#strURL = 'http://emergency.washington.edu/category/emergency-links/?json=1'
+#strURL = 'http://emergency.washington.edu/api/get_category_posts/?id=8' 
 oFile = urllib2.urlopen(strURL)
 oJ = json.loads( oFile.read() )
 
-#print json.dumps( oJ )
-
-for post in oJ['posts']:
-    print """Title: %s (%s)""" % (post['title'],post['status'])
+print json.dumps( oJ, sort_keys=True, indent=4) 
+#print oJ['pages']
