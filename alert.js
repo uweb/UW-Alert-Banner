9f5a5aaab1df6997f7f395e2bb8a3a2c7081161b
Chris Heiland <cheiland@uw.edu>
2012-05-25 15:01:19 -0700

    First iteration of .com support

---
+++ alert.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+  var strURL = 'alert.json';
+  $.getJSON(strURL, function(data) {
+    // console.log(data.posts[0].categories);
+
+    // Alert colors
+    types = {
+        'red-alert'     : 1,
+        'orange-alert'  : 1,
+        'blue-alert'    : 1,
+        'steel-alert'   : 1
+    };
+ 
+    $.each(data.posts[0].categories, function(key,val) {
+      if (types[val.slug]) {
+          // Fire up alert
+      }
+    });
+
+    // data.categories
+    // var items = [];
+  
+    // $.each(data, function(key, val) {
+    //   items.push('<li id="' + key + '">' + val + '</li>');
+    // });
+  
+    // $('<ul/>', {
+    //   'class': 'my-new-list',
+    //   html: items.join('')
+    // }).appendTo('body');
+  });
+
+});
+
+
+/*  University of Washington - Alert 2.0 Beta
+ *  (c) 2011 Chris Heiland, Tim Chang-Miller
+ *
+ *  Script should be included like such:
+ * 
+ *  <html>
+ *  <head>
+ *  <title>Page Title</title>
+ *  <script type="text/javascript" src="http://emergency.washington.edu/alert.js"></script>
+ *  </head>
+ *  <body>
+ * 
+ *  <script type="text/javascript">
+ *    displayAlert();
+ *  </script>
+ *  </body>
+ *  </html>
+ *
+ *  Full docs at:
+ *  uw.edu/externalaffairs/uwmarketing/toolkits/uw-alert-banner/
+ *
+ *--------------------------------------------------------------------------*/
+
+var strCSS = document.createElement('link');
+strCSS.setAttribute('href', '//www.washington.edu/static/uwalert.css');
+strCSS.setAttribute('rel','stylesheet');
+strCSS.setAttribute('type','text/css');
+$(strCSS).appendTo('head');
+
+// addElement - display HTML on page right below the body page
+// don't want the alert to show up randomly
+function addElement(strAlertTitle,strAlertLink,strAlertMessage)
+{
+  var wrapperDiv = document.createElement('div');
+  wrapperDiv.setAttribute('id','alertMessage');
+
+  var alertBoxDiv = document.createElement('div');
+  alertBoxDiv.setAttribute('id', 'alertBox');
+
+  var alertBoxTextDiv = document.createElement('div');
+  alertBoxTextDiv.setAttribute('id', 'alertBoxText');
+
+  var header1 = document.createElement('h1');
+  var header1Text = document.createTextNode('Campus Alert:');
+  header1.appendChild(header1Text);
+
+  var alertTextP = document.createElement('p');
+  var alertText = document.createTextNode(strAlertMessage);
+  alertTextP.appendChild(alertText);
+
+  var alertLink = document.createElement('a');
+  alertLink.setAttribute('href', strAlertLink);
+  alertLink.setAttribute('title', strAlertTitle);
+  var alertLinkText = document.createTextNode('More Info');
+  alertLink.appendChild(alertLinkText);
+
+  var gtText = document.createTextNode(' >>');
+
+  var clearDiv = document.createElement('div');
+  clearDiv.setAttribute('id', 'clearer');
+
+  // Start Building the Actual Div
+  alertTextP.appendChild(alertLink);
+  alertTextP.appendChild(gtText);
+
+  alertBoxTextDiv.appendChild(header1);
+  alertBoxTextDiv.appendChild(alertTextP);
+
+  alertBoxDiv.appendChild(alertBoxTextDiv);
+  alertBoxDiv.appendChild(clearDiv);
+
+  wrapperDiv.appendChild(alertBoxDiv);
+
+  // Grab the tag to start the party
+  var bodyTag = document.getElementsByTagName('body')[0];
+
+  bodyTag.style.margin = '0px';
+  bodyTag.style.padding = '0px';
+
+  bodyTag.insertBefore(wrapperDiv, bodyTag.firstChild);
+}
+
+// displayAlert - grab content to display message 
+// function displayAlert()
+// {
+    var strAlertTitle = '';
+    var strAlertLink = 'http://emergency.washington.edu/';
+    var strAlertMessage = '';
+
+    addElement(strAlertTitle,strAlertLink,strAlertMessage);
+// }
