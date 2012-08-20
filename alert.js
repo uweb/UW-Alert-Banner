2a203e1430da55f4caf2851c73ae5b1ea5340bd9
Chris Heiland <cheiland@uw.edu>
2012-08-20 13:01:47 -0700

    load jquery if not available

--- alert.js
+++ alert.js
@@ -19,67 +19,85 @@
  *
  *--------------------------------------------------------------------------*/
 
-$(document).ready(function() {
-  $.getJSON('alert.cgi', function(data) {
-
-    // Alert colors
-    types = {
-      'red-alert-urgent' : 'red',
-      'orange-alert'     : 'orange',
-      'steel-alert-fyis' : 'steel',
-      'test'             : 'steel',
-    };
- 
-    $.each(data.posts[0].categories, function(strName,objCategory) {
-      if (types[objCategory.slug]) {
-        // Fire up alert
-        var strAlertTitle = data.posts[0].title;
-        var strAlertLink = '//emergency.washington.edu/';
-        var strAlertMessage = data.posts[0].excerpt;
-        var strAlertColor = types[objCategory.slug];
-
-        $('body')
-          .css({
-            'margin'  : '0px',
-            'padding' : '0px'
-          })
-          .prepend($('<div></div>').attr('id','alertMessage')
-            .append(
-              $('<div></div>')
-                .attr({
-                  'id'    : 'alertBox',
-                  'class' : strAlertColor
-                })
-              .append(
-                $('<div></div>')
-                .attr('id','alertBoxText')
-                .html(
-                  $(strAlertMessage)
-                    .append(' ') // Needed Spacing
-                    .append($('<a></a>')
-                      .attr({
-                          'href'  : strAlertLink,
-                          'title' : strAlertTitle
-                      })
-                      .text('More Info >>')
+var jQueryScriptOutputted = false;
+function initJQuery() {
+    
+  //if the jQuery object isn't available
+  if (typeof(jQuery) == 'undefined') {
+    if (! jQueryScriptOutputted) {
+      //only output the script once..
+      jQueryScriptOutputted = true;
+      
+      //output the script (load it from google api)
+      document.write("<scr" + "ipt type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js\"></scr" + "ipt>");
+    }
+    setTimeout("initJQuery()", 50);
+  } else {
+    $(function() {
+      $.getJSON('alert.cgi', function(data) {
+    
+        // Alert colors
+        types = {
+          'red-alert-urgent' : 'red',
+          'orange-alert'     : 'orange',
+          'steel-alert-fyis' : 'steel',
+          'test'             : 'steel',
+        };
+     
+        $.each(data.posts[0].categories, function(strName,objCategory) {
+          if (types[objCategory.slug]) {
+            // Fire up alert
+            var strAlertTitle = data.posts[0].title;
+            var strAlertLink = '//emergency.washington.edu/';
+            var strAlertMessage = data.posts[0].excerpt;
+            var strAlertColor = types[objCategory.slug];
+    
+            $('body')
+              .css({
+                'margin'  : '0px',
+                'padding' : '0px'
+              })
+              .prepend($('<div></div>').attr('id','alertMessage')
+                .append(
+                  $('<div></div>')
+                    .attr({
+                      'id'    : 'alertBox',
+                      'class' : strAlertColor
+                    })
+                  .append(
+                    $('<div></div>')
+                    .attr('id','alertBoxText')
+                    .html(
+                      $(strAlertMessage)
+                        .append(' ') // Needed Spacing
+                        .append($('<a></a>')
+                          .attr({
+                              'href'  : strAlertLink,
+                              'title' : strAlertTitle
+                          })
+                          .text('More Info >>')
+                        )
                     )
+                    .prepend($('<h1></h1>').html('Campus Alert: '))
+                  )
+                  .append($('<div></div>').attr('id','clearer'))
                 )
-                .prepend($('<h1></h1>').html('Campus Alert: '))
-              )
-              .append($('<div></div>').attr('id','clearer'))
-            )
-          );
-      }
+              );
+          }
+        });
+      });
+    
+      // Add some custom styles for the banner
+      $('head').append($('<link>')
+        .attr({ 
+          'href' : '//www.washington.edu/static/uwalert.css',
+          'rel'  : 'stylesheet',
+          'type' : 'text/css'
+        })
+      );
+    
     });
-  });
+  }
+}
 
-  // Add some custom styles for the banner
-  $('head').append($('<link>')
-    .attr({ 
-      'href' : '//www.washington.edu/static/uwalert.css',
-      'rel'  : 'stylesheet',
-      'type' : 'text/css'
-    })
-  );
-
-});
+initJQuery();
