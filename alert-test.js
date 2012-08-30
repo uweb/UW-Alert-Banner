f26cfdb3bb743774b79fa0ea3e5194647c1e5949
Chris Heiland <cheiland@uw.edu>
2012-08-30 16:09:28 -0700

    test alert in place

--- alert-test.js
+++ alert-test.js
@@ -19,93 +19,103 @@
  *
  *--------------------------------------------------------------------------*/
 
-var jQueryScriptOutputted = false;
-function initJQuery() {
-    
-  //if the jQuery object isn't available
-  if (typeof(jQuery) == 'undefined') {
-    if (! jQueryScriptOutputted) {
-      //only output the script once..
-      jQueryScriptOutputted = true;
-      
-      //output the script (load it from google api)
-      document.write("<scr" + "ipt type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js\"></scr" + "ipt>");
-    }
-    setTimeout("initJQuery()", 50);
-  } else {
-    jQuery(function($) {
-      $.ajax({
-        url: '//www.washington.edu/static/alert-test.json?callback=?', 
-        dataType: 'jsonp',
-        crossDomain: true,
-        success: function(data) {
-          // Alert colors
-          types = {
-            'red-alert-urgent' : 'red',
-            'orange-alert'     : 'orange',
-            'steel-alert-fyis' : 'steel',
-            'test'             : 'steel'
-          };
+var strProto = (window.location.protocol == 'https:') ? 'https://' : 'http://';
 
-          // Because we don't always have alerts
-          if (data.posts.length == 0) {
-            return false;
-          }
-     
-          $.each(data.posts[0].categories, function(strName,objCategory) {
-            if (types[objCategory.slug]) {
-              // Fire up alert
-              var strAlertTitle = data.posts[0].title;
-              var strAlertLink = '//emergency.washington.edu/';
-              var strAlertMessage = data.posts[0].excerpt;
-              var strAlertColor = types[objCategory.slug];
+// Code contributed by Dustin Brewer
+var strCSS = document.createElement('link');
+strCSS.setAttribute('href', strProto + 'www.washington.edu/static/uwalert.css');
+strCSS.setAttribute('rel','stylesheet');
+strCSS.setAttribute('type','text/css');
+document.getElementsByTagName('head')[0].appendChild(strCSS);
+
+// displayAlert - grab content to display message 
+function displayAlert()
+{
+    // Alert colors
+    types = {
+      'red-alert-urgent' : 'red',
+      'orange-alert'     : 'orange',
+      'steel-alert-fyis' : 'steel',
+    };
     
-              $('body')
-                .css({
-                  'margin'  : '0px',
-                  'padding' : '0px'
-                })
-                .prepend($('<div></div>').attr('id','alertMessage')
-                  .append(
-                    $('<div></div>')
-                      .attr({
-                        'id'    : 'alertBox',
-                        'class' : strAlertColor
-                      })
-                    .append(
-                      $('<div></div>')
-                      .attr('id','alertBoxText')
-                      .html(
-                        $(strAlertMessage)
-                          .append(' ') // Needed Spacing
-                          .append($('<a></a>')
-                            .attr({
-                                'href'  : strAlertLink,
-                                'title' : strAlertTitle
-                            })
-                            .text('More Info >>')
-                          )
-                      )
-                      .prepend($('<h1></h1>').html('Campus Alert: '))
-                    )
-                    .append($('<div></div>').attr('id','clearer'))
-                  )
-                ); // end $('body')
-            } // end .slug 
-          }); // end $.each
-    
-          // Add some custom styles for the banner
-          $('head').append( $('<link>')
-            .attr({ 
-              'href' : '//www.washington.edu/static/uwalert.css',
-              'rel'  : 'stylesheet',
-              'type' : 'text/css'
-            })
-          ); // end $('head')
-        } // end .success
-      }); // end $.ajax
-    });   
-  }
+    var strAlertTitle = 'Campus\ Alert\:';
+    var strAlertLink = 'http://emergency.washington.edu/';
+    var strAlertMessage = 'There\ is\ no\ emergency\.\ \ For\ additional\ information\,\ please\ visit\ ';
+    var strAlertColor = 'steel';
+        
+    // Banners must have an actual color
+    if (strAlertColor)
+    {
+        addElement(strAlertTitle,strAlertLink,strAlertColor,strAlertMessage);
+    }
 }
 
-initJQuery();
+// addElement - display HTML on page right below the body page
+// don't want the alert to show up randomly
+function addElement(strAlertTitle,strAlertLink,strAlertColor,strAlertMessage)
+{
+  // Grab the tag to start the party
+  var bodyTag = document.getElementsByTagName('body')[0];
+  
+  bodyTag.style.margin = '0px';
+  bodyTag.style.padding = '0px';
+
+  var wrapperDiv = document.createElement('div');
+  wrapperDiv.setAttribute('id','alertMessage');
+
+  var alertBoxDiv = document.createElement('div');
+  alertBoxDiv.setAttribute('id', 'alertBox');
+  alertBoxDiv.setAttribute('class', strAlertColor);
+
+  var alertBoxTextDiv = document.createElement('div');
+  alertBoxTextDiv.setAttribute('id', 'alertBoxText');
+  
+  var header1 = document.createElement('h1');
+  var header1Text = document.createTextNode('Campus Alert:');
+  header1.appendChild(header1Text);
+
+  var alertTextP = document.createElement('p');
+
+  // Wordpres includes html, this won't do
+  // var alertText = document.createTextNode(strAlertMessage);
+  // alertTextP.appendChild(alertText);
+
+  var div = document.createElement("div");
+  div.innerHTML = strAlertMessage;
+  // Strip out html that wordpress.com gives us
+  var alertTextMessage = div.textContent || div.innerText || "";
+  // Build alert text node and cut of max characters
+  var alertText = document.createTextNode(
+    alertTextMessage.substring(0,200) + 
+    (alertTextMessage.length >= 200 ? '... ' : ' ')
+  );
+  alertTextP.appendChild(alertText);
+
+
+  var alertLink = document.createElement('a');
+  alertLink.setAttribute('href', strAlertLink);
+  alertLink.setAttribute('title', strAlertTitle);
+  var alertLinkText = document.createTextNode('More Info');
+  alertLink.appendChild(alertLinkText);
+
+  var gtText = document.createTextNode(' >>');
+  
+  var clearDiv = document.createElement('div');
+  clearDiv.setAttribute('id', 'clearer');
+
+  // Start Building the Actual Div
+  alertTextP.appendChild(alertLink);
+  alertTextP.appendChild(gtText);
+
+  alertBoxTextDiv.appendChild(header1);
+  alertBoxTextDiv.appendChild(alertTextP);
+
+  alertBoxDiv.appendChild(alertBoxTextDiv);
+  alertBoxDiv.appendChild(clearDiv);
+
+  wrapperDiv.appendChild(alertBoxDiv);
+  
+  bodyTag.insertBefore(wrapperDiv, bodyTag.firstChild);
+} 
+
+displayAlert();
--- alert.js
+++ alert.js
@@ -9,7 +9,6 @@
  *  </head>
  *  <body>
  * 
- *  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
  *  <script type="text/javascript" src="//washington.edu/static/alert.js"></script>
  *  </body>
  *  </html>
@@ -19,11 +18,10 @@
  *
  *--------------------------------------------------------------------------*/
 
-
 var strProto = (window.location.protocol == 'https:') ? 'https://' : 'http://';
 
 var strScript = document.createElement('script');
-// script.setAttribute('src', '//www.washington.edu/static/alert-test.json');
+// Using the wordpress callback to grab our json
 strScript.setAttribute('src', strProto + 'public-api.wordpress.com/rest/v1/sites/uwemergency.wordpress.com/posts/?number=1&type=post&status=publish&callback=displayAlert');
 document.getElementsByTagName('head')[0].appendChild(strScript); 
 
@@ -37,7 +35,6 @@ document.getElementsByTagName('head')[0].appendChild(strCSS);
 // displayAlert - grab content to display message 
 function displayAlert(data)
 {
-    // console.log(data);
     // We don't care if there's nothing
     if (data.found == 0) 
     {
@@ -49,13 +46,12 @@ function displayAlert(data)
       'red-alert-urgent' : 'red',
       'orange-alert'     : 'orange',
       'steel-alert-fyis' : 'steel',
-      'test'             : 'steel'
     };
     
     for (strCategory in data.posts[0].categories ) 
     {
         var objCategory = data.posts[0].categories[strCategory]
-        // console.log(objCategory);
+        // Quick way to determine color
         if (types[objCategory.slug]) 
         {
             var strAlertTitle  = data.posts[0].title;
@@ -65,6 +61,7 @@ function displayAlert(data)
         }
         
     }
+    // Banners must have an actual color
     if (strAlertColor)
     {
         addElement(strAlertTitle,strAlertLink,strAlertColor,strAlertMessage);
@@ -97,6 +94,7 @@ function addElement(strAlertTitle,strAlertLink,strAlertColor,strAlertMessage)
 
   var alertTextP = document.createElement('p');
 
+  // Wordpres includes html, this won't do
   // var alertText = document.createTextNode(strAlertMessage);
   // alertTextP.appendChild(alertText);
 
@@ -137,5 +135,3 @@ function addElement(strAlertTitle,strAlertLink,strAlertColor,strAlertMessage)
   
   bodyTag.insertBefore(wrapperDiv, bodyTag.firstChild);
 } 
-
-// displayAlert();
