483d8649e37536ac20dea9c15dce475ae374c2ba
Chris Heiland <cheiland@uw.edu>
2012-10-01 10:24:30 -0700

    removing unnecessary header

--- alert.py
+++ alert.py
@@ -23,7 +23,7 @@ class AlertBanner(object):
     Primary Alert Object
     """
     def __init__(self):
-        self._url = 'https://public-api.wordpress.com/rest/v1/sites/uwemergency.wordpress.com/posts/?number=1&type=post&status=publish'
+        self._url = 'http://public-api.wordpress.com/rest/v1/sites/uwemergency.wordpress.com/posts/?number=1&type=post&status=publish'
         self._alertdata = ""
         self._status = ""
         self._alert = ""
@@ -104,114 +104,6 @@ class AlertBanner(object):
             # TODO: Is there a reason to save every time?
             #self._save(json.dumps(self._alertdata, sort_keys=True, indent=4),'alert.json')
 
-    def _build(self):
-        ## If we have an color, we are running an alert - we need
-        ## this to be as streamlined as possible
-
-        strHeader = """/*  University of Washington - Alert 2.0 Beta
- *  (c) 2011 Chris Heiland, Tim Chang-Miller
- *
- *  Script should be included like such:
- * 
- *  <html>
- *  <head>
- *  <title>Page Title</title>
- *  <script type="text/javascript" src="http://emergency.washington.edu/alert.js"></script>
- *  </head>
- *  <body>
- * 
- *  <script type="text/javascript">
- *    displayAlert();
- *  </script>
- *  </body>
- *  </html>
- *
- *  Full docs at:
- *  uw.edu/externalaffairs/uwmarketing/toolkits/uw-alert-banner/
- *
- *--------------------------------------------------------------------------*/
-"""
-
-        if self.color:
-            strContent = """// Code contributed by Dustin Brewer
-var strProto = (window.location.protocol == 'https:') ? 'https://' : 'http://';
-var strCSS = document.createElement('link');
-strCSS.setAttribute('href', strProto + 'emergency.washington.edu/uwalert.css');
-strCSS.setAttribute('rel','stylesheet');
-strCSS.setAttribute('type','text/css');
-document.getElementsByTagName('head')[0].appendChild(strCSS);
-// addElement - display HTML on page right below the body page
-// don't want the alert to show up randomly
-function addElement(strAlertTitle,strAlertLink,strAlertMessage)
-{
-  // Grab the tag to start the party
-  var bodyTag = document.getElementsByTagName('body')[0];
-
-  bodyTag.style.margin = '0px';
-  bodyTag.style.padding = '0px';
-
-  var wrapperDiv = document.createElement('div');
-  wrapperDiv.setAttribute('id','alertMessage');
-
-  var alertBoxDiv = document.createElement('div');
-  alertBoxDiv.setAttribute('id', 'alertBox');
-  alertBoxDiv.setAttribute('class', '%s');
-
-  var alertBoxTextDiv = document.createElement('div');
-  alertBoxTextDiv.setAttribute('id', 'alertBoxText');
-
-  var header1 = document.createElement('h1');
-  var header1Text = document.createTextNode('Campus Alert:');
-  header1.appendChild(header1Text);
-
-  var alertTextP = document.createElement('p');
-  var alertText = document.createTextNode(strAlertMessage);
-  alertTextP.appendChild(alertText);
-
-  var alertLink = document.createElement('a');
-  alertLink.setAttribute('href', strAlertLink);
-  alertLink.setAttribute('title', strAlertTitle);
-  var alertLinkText = document.createTextNode('More Info');
-  alertLink.appendChild(alertLinkText);
-
-  var gtText = document.createTextNode(' >>');
-
-  var clearDiv = document.createElement('div');
-  clearDiv.setAttribute('id', 'clearer');
-
-  // Start Building the Actual Div
-  alertTextP.appendChild(alertLink);
-  alertTextP.appendChild(gtText);
-
-  alertBoxTextDiv.appendChild(header1);
-  alertBoxTextDiv.appendChild(alertTextP);
-
-  alertBoxDiv.appendChild(alertBoxTextDiv);
-  alertBoxDiv.appendChild(clearDiv);
-
-  wrapperDiv.appendChild(alertBoxDiv);
-
-  bodyTag.insertBefore(wrapperDiv, bodyTag.firstChild);
-}
-
-// displayAlert - grab content to display message 
-function displayAlert()
-{
-    var strAlertTitle = '%s';
-    var strAlertLink = '%s';
-    var strAlertMessage = '%s';
-
-    addElement(strAlertTitle,strAlertLink,strAlertMessage);
-}
-""" % (self.color, re.escape(self.alert['title']), self._link, re.escape(self._excerpt))
-        else:
-            strContent = """function displayAlert(strMode)
-{
-    // Does nothing useful (for error & warning prevention)
-}
-"""
-        self.output = strHeader + strContent
-        return 1
 
     def _save(self,sData,sFile):
         """
@@ -244,12 +136,6 @@ function displayAlert()
         ## Exists solely for tests
         if sType == 'plain':
             return strPlain
-
-        ## if self._build():
-        ##     self._save(self.output,"""%s.js""" % self._filename)
-        ##     return self.output
-        ## else:
-        ##     print "Problem rendering banner\n"
 
 def main():
     """
