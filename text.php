ab2cb592732d3915d5945b4e281992b431793a37
Chris Heiland <cheiland@uw.edu>
2012-12-28 12:02:36 -0800

    Adding in first iteration

---
+++ text.php
@@ -0,0 +1,25 @@
+<?php 
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+// We want to deliver a overkill static text file
+header( 'Content-Type: plain/text, charset=utf-8' );
+
+$strURL = 'http://www.washington.edu/static/UW-Alert-Banner/alert/?test=true';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $strURL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// TODO Watch timeout and adjust as needed
+curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$strData = curl_exec($ch);
+curl_close($ch);
+
+$strDataDecoded = json_decode($strData);
+
+// var_dump($strDataDecoded);
+
+printf("%s<break />\n%s", $strDataDecoded->{'posts'}[0]->{'title'}, $strDataDecoded->{'posts'}[0]->{'excerpt'});
+?>
